f287c8bb497e2611a5d7ee0990ab07cf89c3ee58
Kai Wayman <botd64@gmail.com>
2025-04-05 17:02:53 -0400

    UpdatedVoice

--- backend/voice.py
+++ backend/voice.py
@@ -10,6 +10,12 @@ embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validat
 speaker_embedding = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
 # You can replace this embedding with your own as well.
 
-speech = synthesiser("As we begin this meditation, I invite you to take a deep breath in through", forward_params={"speaker_embeddings": speaker_embedding})
+# Read the text from the "voiceText.txt" file
+with open("voiceText.txt", "r") as file:
+    text = file.read()
+
+speech = synthesiser(text, forward_params={"speaker_embeddings": speaker_embedding})
 
 sf.write("speech.wav", speech["audio"], samplerate=speech["sampling_rate"])
+
+
