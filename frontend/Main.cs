f4303786a9ef83fd3ea34eaf2df87229120456a0
AnotherGuy7 <111369234+Jayl01@users.noreply.github.com>
2025-04-05 18:15:31 -0400

    FrontEnd Base

--- frontend/Main.cs
+++ frontend/Main.cs
@@ -1,7 +1,23 @@
 using Godot;
 using System;
+using System.Xml.Linq;
 
-public partial class Main : Node3D
+public partial class Main : Node
 {
+	private Texture wallTexture;
+	private MeshInstance3D wallInstance;
 
+	public override void _Ready()
+	{
+		string pngDir = "C:\\Contents\\Projects\\Hackathons\\Meditation\\frontend\\Images\\wallTexture.png";
+		Image image = Image.LoadFromFile(pngDir);
+		StandardMaterial3D newTextureMaterial = new StandardMaterial3D();
+		newTextureMaterial.AlbedoTexture = ImageTexture.CreateFromImage(image);
+		GetChild<MeshInstance3D>(4).SetSurfaceOverrideMaterial(0, newTextureMaterial);
+	}
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+	}
 }
