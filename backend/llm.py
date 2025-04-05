afb2828c41e2cadb890527fe8feab42c36ffa6e8
kusf <130384593+kusf@users.noreply.github.com>
2025-04-05 14:21:29 -0400

    Update llm.py

--- backend/llm.py
+++ backend/llm.py
@@ -1,10 +1,15 @@
 from ollama import chat
 from ollama import ChatResponse
 
-response: ChatResponse = chat(model='llama3', messages=[
-  {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
-  },
-])
-print(response['message']['content'])
+response: ChatResponse = chat(
+    model="llama3",
+    messages=[
+        {
+            "role": "assistant",
+            "content": (
+                "You are a slighly hypnotic meditation guru. Help the user find his inner peace by determining how they feel and suggesting moods and activities. "
+            ),
+        },
+    ],
+)
+print(response["message"]["content"])
