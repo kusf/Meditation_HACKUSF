ca1112ecdfc40842cb3b02ebe4c23a29bf68b51e
kusf <130384593+kusf@users.noreply.github.com>
2025-04-06 07:15:37 -0400

    Updated

--- backend/llm.py
+++ backend/llm.py
@@ -5,15 +5,13 @@ from ollama import ChatResponse
 
 class LLM:
     def __init__(self):
-        self.client = Client(
-            host="http://localhost:11434"
-        )
+        self.client = Client(host="http://localhost:11434")
         response: ChatResponse = self.client.chat(
             model="model",
             messages=[
                 {
                     "role": "user",
-                    "content": "Hello, I am a new person. Please give me a new adventure and options!",
+                    "content": "Hello, I am a new person. Please give me a new adventure and <options>!",
                 },
             ],
         )
@@ -32,13 +30,25 @@ class LLM:
         self.data = self.formatted_return(response["message"]["content"])
 
     def formatted_return(self, input) -> dict:
-        return {
-            "text": input[: input.index("<option>")],
-            "options": re.findall(r"<option>(.+)</option>", input),
-            "image description": re.findall(r"<image>(.+)</image>", input)[0],
-        }
+        try:
+            return {
+                "text": input[: input.index("<option>")],
+                "options": re.findall(r"<option>(.+)</option>", input),
+                "image description": re.findall(r"<image>(.+)</image>", input)[0],
+                "intensity": re.findall(r"<intensity>(.+)</intensity>", input)[0],
+            }
+        except re.PatternError:
+            self.__call__(
+                "Please include a responde that has <options> <image> and <intensity>"
+            )
+            return {
+                "text": input[: input.index("<option>")],
+                "options": re.findall(r"<option>(.+)</option>", input),
+                "image description": re.findall(r"<image>(.+)</image>", input)[0],
+                "intensity": re.findall(r"<intensity>(.+)</intensity>", input)[0],
+            }
 
-    #returns the LLM response
+    # returns the LLM response
     def get_data(self):
         return self.data
 
